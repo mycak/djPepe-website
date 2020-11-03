589c564531fa19375f30734adf2b1bc57703cf49
mycak <mycak@wp.pl>
2020-11-03 19:29:56 +0100

    home page hero

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,21 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 import Navigation from './Navigation'
 import Footer from './Footer'
-import GlobalStyles from '../styles/GlobalStyles'
 import 'normalize.css'
+import GlobalStyles from '../styles/GlobalStyles'
 
 const ContentStyles = styled.div`
   width: 100%;
   min-height: 90vh;
 `
+const Layout = ({ children }) => (
+  <>
+    <GlobalStyles />
+    <Navigation />
+    <ContentStyles>{children}</ContentStyles>
+    <Footer />
+  </>
+)
 
-export default function Layout({ children }) {
-  return (
-    <>
-      <GlobalStyles />
-      <Navigation />
-      <ContentStyles>{children}</ContentStyles>
-      <Footer />
-    </>
-  )
-}
+export default Layout

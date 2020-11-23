b978b2ec79a1f945ba3a7de6ab2ac557bb6600bb
mycak <mycak@wp.pl>
2020-11-23 15:29:56 +0100

    favicon changed

--- src/pages/SEO.js
+++ src/pages/SEO.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import favicon from '../assets/images/favicon.ico'
 
 const SEO = () => (
   <Helmet titleTemplate="Dj Pepe">
     <html lang="en" />
+    <link rel="icon" href={favicon} />
     <title>Dj Pepe</title>
   </Helmet>
 )
